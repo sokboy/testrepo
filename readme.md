80a72eab5da33c64bf53fda324f4e7a45fac7cd9
sokboy <johnsokaris@gmail.com>
2022-09-05 16:41:22 -0400

    attempt1

---
+++ readme.md
@@ -0,0 +1,2 @@
+# test readme: John Sokaris
+This is my first attempt at a readme.md file
---
+++ some_code.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main()
+{
+int numbers[] = {10,9,8,7,6,5,4,3,2,1};
+	for(int i=0; i<10;i++)
+	{printf("%d\n", numbers[i]);}
+return 0;
+}
